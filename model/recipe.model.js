a1aaf120e9a300a046833e5045f2a20a0149c7b9
TolgaElbir <telbir@student.avans.nl>
2017-11-23 20:03:44 +0100

    .

--- model/recipe.model.js
+++ model/recipe.model.js
@@ -21,13 +21,13 @@ const Recipe = mongoose.model('recipe', RecipeSchema);
 
 // Add a 'dummy' user (every time you require this file!)
 const recipe = new Recipe({
-    name: 'test',
-    title: 'test',
-    recipes: [
-        { name: 'test', ingredients: [
-            { name: 'test', weight: 200, price: "test" },
-        ]}
-        ]
+    name: 'kroket',
+    description: 'Lekkere kroket',
+    imagePath: 'http://moniquevandervloed.nl/wp-content/uploads/2017/05/kroket1.jpg',
+    ingredients: [
+        { name: 'vlees', amount: 1000 },
+        { name: 'korst', amount: 20}
+    ]
 }).save();
 
 module.exports = Recipe;
